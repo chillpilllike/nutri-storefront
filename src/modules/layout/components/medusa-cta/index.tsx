a14add0ee2eab77082e1c3c252c72371fe2dfda6
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-27 12:46:07 +0530

    Update index.tsx

--- src/modules/layout/components/medusa-cta/index.tsx
+++ src/modules/layout/components/medusa-cta/index.tsx
@@ -1,4 +1,5 @@
-import { Text } from "@medusajs/ui"
+import { Text, IconBadge } from "@medusajs/ui"
+import { Stripe, Mastercard, Visa, Klarna, VerifiedBadge } from "@medusajs/icons"
 
 import Medusa from "../../../common/icons/medusa"
 import NextJs from "../../../common/icons/nextjs"
