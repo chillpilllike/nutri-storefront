744b502e1651bfdc9ce8bcd9797ca50bc34681cb
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-27 13:34:26 +0530

    Update index.tsx

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -49,8 +49,14 @@ export default async function Footer() {
                 href="/"
                 className="txt-compact-xlarge-plus text-ui-fg-subtle hover:text-ui-fg-base uppercase"
               >
-                Medusa Store
+                About Us - Nutricity
               </LocalizedClientLink>
+              <p className="mt-2 text-ui-fg-subtle text-sm">
+                At Nutricity, we believe that health and wellness should be accessible to everyone. As Australia’s trusted destination for premium supplements, we are committed to providing high-quality, science-backed products to support your journey towards a healthier, more vibrant life.
+              </p>
+              <p className="mt-2 text-ui-fg-subtle text-sm">
+                Email: support@SecretGreen.com.au
+              </p>
             </div>
             <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
               {product_categories && product_categories.length > 0 && (
