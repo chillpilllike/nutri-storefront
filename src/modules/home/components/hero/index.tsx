71cae87d7e098ff81ee0eef985cc9c3d9934f159
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-27 13:24:54 +0530

    Update index.tsx

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -3,34 +3,35 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div
+      className="h-[75vh] w-full border-b border-ui-border-base relative bg-cover bg-center"
+      style={{
+        backgroundImage: "url('https://nutricity.com.au/cdn/shop/files/supps_by_nutricity_1600x.png')", // Add the background image here
+      }}
+    >
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Ecommerce Starter Template
+            Unique Products, Exceptional Quality & Timely Delivery
           </Heading>
           <Heading
-            level="h2"
+            level="h3"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Powered by Medusa and Next.js
+            Free Shipping | 30 Days Return | Secure payments
           </Heading>
         </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
+        <a href="/store" target="_blank">
           <Button variant="secondary">
-            View on GitHub
-            <Github />
+            Shop the Latest Trends!
           </Button>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
